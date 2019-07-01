370c8c3c75c215b2f3b4e2594356cb98f9f631c0
Song <503027010@logon.ds.ge.com>
2019-07-01 12:02:39 +0800

    urls.

--- wagtail/blogsite/blogsite/urls.py
+++ wagtail/blogsite/blogsite/urls.py
@@ -10,10 +10,8 @@ from search import views as search_views
 
 urlpatterns = [
     url(r'^django-admin/', admin.site.urls),
-
     url(r'^admin/', include(wagtailadmin_urls)),
     url(r'^documents/', include(wagtaildocs_urls)),
-
     url(r'^search/$', search_views.search, name='search'),
 
     # For anything not caught by a more specific rule above, hand over to
@@ -26,11 +24,11 @@ urlpatterns = [
     #    url(r'^pages/', include(wagtail_urls)),
 ]
 
-
 if settings.DEBUG:
     from django.conf.urls.static import static
     from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
     # Serve static and media files from development server
     urlpatterns += staticfiles_urlpatterns()
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
