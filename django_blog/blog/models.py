7c933da02ddd5223469d6994a6eecff96cdf460e
Song <503027010@logon.ds.ge.com>
2019-07-17 11:30:30 +0800

    add delete

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -92,12 +92,10 @@ class Post(models.Model):
         # 如果没有填写摘要
         if not self.excerpt:
             # 首先实例化一个 Markdown 类，用于渲染 body 的文本
-            md = markdown.Markdown(
-                extensions=[
-                    "markdown.extensions.extra",
-                    "markdown.extensions.codehilite",
-                ]
-            )
+            md = markdown.Markdown(extensions=[
+                "markdown.extensions.extra",
+                "markdown.extensions.codehilite",
+            ])
             # 先将 Markdown 文本渲染成 HTML 文本
             # strip_tags 去掉 HTML 文本的全部 HTML 标签
             # 从文本摘取前 54 个字符赋给 excerpt
@@ -105,3 +103,6 @@ class Post(models.Model):
 
         # 调用父类的 save 方法将数据保存到数据库中
         super(Post, self).save(*args, **kwargs)
+
+    def delete(self, *args, **kwargs):
+        super(Post, self).delete(*args, **kwargs)
